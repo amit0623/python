68c9d0a4ca8254fa48d46c25da13a2526e891d95
Amit Kumar <akumar626@gatech.edu>
2020-08-11 07:56:05 +0530

    modified rate

--- Diet/decimal_demo.py
+++ Diet/decimal_demo.py
@@ -3,7 +3,7 @@
 from decimal import Decimal
 principal =  Decimal('100000')
 rate = Decimal('0.05')
-
+print ("rate =",rate)
 # Do calculation among decimal objects.
 
 # Add / Sub / Mult is allowed 
@@ -11,4 +11,3 @@ rate = Decimal('0.05')
 for year in range ( 1 , 11 ):
     amount = principal* (1 + rate) ** year # New Prin amount 
     print(f'{year:>2}{amount:>10.2f}')
-     
